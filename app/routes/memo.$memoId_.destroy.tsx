1be05afba18875a7b92964b1bd107240bb33e6bc
ykob <info@tplh.net>
2023-10-08 02:40:05 +0900

    update destroy memo action.

--- app/routes/memo.$memoId_.destroy.tsx
+++ app/routes/memo.$memoId_.destroy.tsx
@@ -1,11 +1,14 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import invariant from "tiny-invariant";
+import { deleteMemo } from "~/data";
 
-import { deleteMemo } from "../data";
+export const loader = () => {
+  return redirect("/");
+};
 
 export const action = async ({ params }: ActionFunctionArgs) => {
   invariant(params.memoId, "Missing memoId param");
   await deleteMemo(params.memoId);
-  return redirect("/");
+  return json({ success: true });
 };
