b97134dc6a1bc00e2c2cbb216944ef011b64ff33
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-10-17 17:58:16 +0900

    fixed the first test.

--- test/routes/index.test.tsx
+++ test/routes/index.test.tsx
@@ -1,8 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { MantineProvider } from "@mantine/core";
 import { render, screen } from "@testing-library/react";
 import Index from "../../app/routes/_index";
 
 it("render the page", () => {
-  render(<Index />);
+  render(
+    <MantineProvider>
+      <Index />
+    </MantineProvider>
+  );
 
   expect(screen.getByRole("heading")).toHaveTextContent(
     "Learn Remix with Mantine"
