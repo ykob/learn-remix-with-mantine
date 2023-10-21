623b78d71d5e0b4e863cad1704dd6b2af8c82d38
ykob <info@tplh.net>
2023-10-21 19:47:36 +0900

    added links on home.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
-import { Container, Title } from "@mantine/core";
+import { Card, Container, Grid, Title } from "@mantine/core";
 import { type MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,6 +15,22 @@ export default function Index() {
       <Title mb="xl" c="white">
         Learn Remix with Mantine
       </Title>
+      <Grid columns={12} gutter="sm">
+        <Grid.Col span={{ base: 6, sm: 4, md: 3 }}>
+          <Link to="/drawer">
+            <Card padding="lg" shadow="sm">
+              Drawer
+            </Card>
+          </Link>
+        </Grid.Col>
+        <Grid.Col span={{ base: 6, sm: 4, md: 3 }}>
+          <Link to="/memo">
+            <Card padding="lg" shadow="sm">
+              Memo
+            </Card>
+          </Link>
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
